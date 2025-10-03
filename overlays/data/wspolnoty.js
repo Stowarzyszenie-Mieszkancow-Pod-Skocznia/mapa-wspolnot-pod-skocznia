603cf700f97b5069fb06ca591892224bfa181d35
Michał Jarosz <mail@michaljarosz.biz>
2025-10-03 15:35:44 +0200

    Wyniesienie danych wspólnot do osobnych plików

---
+++ overlays/data/wspolnoty.js
@@ -0,0 +1,65 @@
+const wspolnotyData = [
+  {
+    name: "City Zen",
+    budynki: "Sikorskiego 9A",
+    przedstawicielWStowarzyszeniu: false
+  },
+  {
+    name: "City Zen II",
+    budynki: "Sikorskiego 9B",
+    przedstawicielWStowarzyszeniu: false
+  },
+  {
+    name: "Dolina Mokotów",
+    budynki: "Bergamotki 6, Eukaliptusowa 4, Lubaszki 9",
+    przedstawicielWStowarzyszeniu: false
+  },
+  {
+    name: "Green Mokotów",
+    administrator: "LemmonHouse",
+    budynki: "Bergamotki 2, Bergamotki 4, Sikorskiego 3, Sikorskiego 3A, Lubaszki 8, Lubaszki 10",
+    przedstawicielWStowarzyszeniu: true,
+    zarzad: "powierzony"
+  },
+  {
+    name: "Green Mokotów 1",
+    www: "https://zarzadzanie-nieruchomosciami.eu/strefa-klienta/123-wspolnota-green-mokotow-1-warszawa",
+    administrator: "PolBest",
+    budynki: "Bergamotki 3, Faworytki 2",
+  },
+  {
+    name: "Green Mokotów 2",
+    administrator: "PolBest",
+    budynki: "Bergamotki 1",
+    www: "https://zarzadzanie-nieruchomosciami.eu/strefa-klienta/63-mokotow/321-wspolnota-green-mokotow-2-warszawa",
+  },
+  {
+    name: "Nordic Mokotów I",
+    przedstawicielWStowarzyszeniu: true,
+    budynki: "Sikorskiego 9C",
+  },
+  {
+    name: "Nordic Mokotów II",
+    przedstawicielWStowarzyszeniu: true,
+    budynki: "Sikorskiego 9D",
+  },
+  {
+    name: "Nordic Mokotów III",
+    przedstawicielWStowarzyszeniu: true,
+    budynki: "Pory 53",
+  },
+  {
+    name: "Vena Mokotów",
+    budynki: "Pory 55, Pory 55A, Pory 57",
+    przedstawicielWStowarzyszeniu: false
+  },
+  {
+    name: "Wille Bergamotki",
+    administrator: "Royal House",
+    budynki: "Eukaliptusowa 1, Eukaliptusowa 3, Eukaliptusowa 5",
+    przedstawicielWStowarzyszeniu: true,
+    zarzad: "właścicielski"
+  }
+]
+
+export {wspolnotyData}
